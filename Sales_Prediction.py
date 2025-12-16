c55c4a5de3e804c293ce16f197c205c403053c66
jaiswalshubhi12 <52368045+jaiswalshubhi12@users.noreply.github.com>
2025-12-16 21:06:22 +0530

    updated

--- Sales_Prediction.py
+++ Sales_Prediction.py
@@ -3,7 +3,8 @@ import pandas as pd
 import numpy as np
 import pickle
 from datetime import datetime
-import sklearn as sk
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_absolute_error
 
 # ---------------------------------------------------
 # Streamlit Page Config + Background Color
